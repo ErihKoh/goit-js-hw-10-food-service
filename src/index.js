63f2c7dce08639ef0221fd91dbc9ae9033111f3f
Yurii Zubar <barsuk0831@gmail.com>
2020-10-09 16:54:32 +0300

    добавил js файлы

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-import menu from "./templates/food_service.hbs";
+import './css/style.css';
+import * as menuMarkup from './js/creat_markup';
+import * as toggleTheme from './js/toggle_theme';
---
+++ src/js/creat_markup.js
@@ -0,0 +1,2 @@
+import menu from './menu.json';
+import foodService from '../templates/food_service.hbs';
